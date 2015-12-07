8e63f692e3319ac89b9f996e43f9a3d45e4a3cbd
Mason X <ohnx@users.noreply.github.com>
2015-12-06 21:44:38 -0500

    Update libBareMetal.h

--- libBareMetal.h
+++ libBareMetal.h
@@ -41,26 +41,26 @@ void b_output_chars(const char *str,unsigned long nbr);
 void b_output(const char *str);
 
 // Index for b_system_config calls
-#define TIMECOUNTER		      0
-#define GET_ARGC		        1
-#define GET_ARGV		        2
-#define NETWORKCALLBACK_GET	3
-#define NETWORKCALLBACK_SET	4
-#define CLOCKCALLBACK_GET	  5
-#define CLOCKCALLBACK_SET	  6
-#define MAC			            30
+#define TIMECOUNTER          0
+#define GET_ARGC             1
+#define GET_ARGV             2
+#define NETWORKCALLBACK_GET  3
+#define NETWORKCALLBACK_SET  4
+#define CLOCKCALLBACK_GET    5
+#define CLOCKCALLBACK_SET    6
+#define MAC                  30
 
 // Index for b_system_misc calls
-#define SMP_GET_ID		   1
-#define SMP_LOCK		     2
-#define SMP_UNLOCK		   3
-#define DEBUG_DUMP_MEM	 4
-#define DEBUG_DUMP_RAX	 5
-#define DELAY			       6
-#define ETHERNET_STATUS	 7
-#define MEM_GET_FREE		 8
-#define SMP_NUMCORES		 9
-#define SMP_QUELEN		   10
+#define SMP_GET_ID       1
+#define SMP_LOCK         2
+#define SMP_UNLOCK       3
+#define DEBUG_DUMP_MEM   4
+#define DEBUG_DUMP_RAX   5
+#define DELAY            6
+#define ETHERNET_STATUS  7
+#define MEM_GET_FREE     8
+#define SMP_NUMCORES     9
+#define SMP_QUELEN       10
 #define RESET            256
 
 #endif
